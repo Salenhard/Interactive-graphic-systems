0c7bd00aeb744c110051ad9775d0cf120f7ae4fe
NemoNology <chicken.gang@mail.ru>
2023-09-20 20:25:42 +0900

    Added class that combines figure and OpenGL

---
+++ Lab-s/1 - Cube/Console-Project/Figure/OpenGLFigure.cs
@@ -0,0 +1,102 @@
+using OpenTK.Graphics.OpenGL4;
+
+namespace Console_Project
+{
+    public class OpenGLDrawFigure
+    {
+        public static readonly int NotInitializatedElementBufferObjectValue = -72;
+        public IFigure Figure { get; private set; }
+        public int VertexBufferObject { get; private set; }
+        public int VertexArrayObject { get; private set; }
+        public int ElementBufferObject { get; private set; }
+        public Shader Shader { get; private set; }
+
+        public OpenGLDrawFigure(IFigure figure, Shader shader)
+        {
+            Figure = figure;
+            ElementBufferObject = NotInitializatedElementBufferObjectValue;
+            Shader = shader;
+        }
+
+        /// <summary>
+        /// Generating buffer from figure as ElementBufferObject and binding that buffer
+        /// </summary>
+        /// <returns> Genarated buffer </returns>
+        public void Init(BufferUsageHint bufferUsageHint = BufferUsageHint.DynamicDraw)
+        {
+            var vertices = Figure.Vertices;
+            var indices = Figure.Indices;
+
+            VertexBufferObject = GL.GenBuffer();
+            GL.BindBuffer(BufferTarget.ArrayBuffer, VertexBufferObject);
+
+            GL.BufferData(
+                BufferTarget.ArrayBuffer,
+                vertices.Length * sizeof(float),
+                vertices,
+                bufferUsageHint
+            );
+
+            VertexArrayObject = GL.GenVertexArray();
+            GL.BindVertexArray(VertexArrayObject);
+
+            GL.VertexAttribPointer(
+                Shader.GetAttribLocation("pos0"),
+                Core.Vec3AttributeSize,
+                VertexAttribPointerType.Float,
+                false,
+                // Because we ara draw triangles!
+                Triangle.VerticesCount * sizeof(float),
+                0
+            );
+            GL.EnableVertexAttribArray(0);
+
+            ElementBufferObject = GL.GenBuffer();
+            GL.BindBuffer(BufferTarget.ElementArrayBuffer, ElementBufferObject);
+            GL.BufferData(
+                BufferTarget.ElementArrayBuffer,
+                indices.Length * sizeof(uint),
+                indices,
+                bufferUsageHint
+            );
+
+            Shader.Use();
+        }
+
+        /// <summary>
+        /// Draw figure using ElementBufferObject
+        /// </summary>
+        public void Draw()
+        {
+            if (ElementBufferObject == NotInitializatedElementBufferObjectValue)
+            {
+                throw new Exception("Trying to draw not initializated figure");
+            }
+
+            Shader.Use();
+            GL.BindVertexArray(VertexArrayObject);
+            GL.DrawElements(
+                PrimitiveType.Triangles,
+                Figure.Indices.Length,
+                DrawElementsType.UnsignedInt,
+                0
+            );
+        }
+
+        public void Dispose()
+        {
+            GL.BindBuffer(BufferTarget.ArrayBuffer, 0);
+            GL.BindVertexArray(0);
+            GL.DeleteBuffer(VertexBufferObject);
+            GL.DeleteBuffer(ElementBufferObject);
+            GL.DeleteVertexArray(VertexArrayObject);
+            Shader.Dispose();
+            ElementBufferObject = NotInitializatedElementBufferObjectValue;
+        }
+
+        ~OpenGLDrawFigure()
+        {
+            Dispose();
+        }
+    }
+}
