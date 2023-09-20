b1d44134a852502b05b7d4be02d5c4c7bf513b41
NemoNology <chicken.gang@mail.ru>
2023-09-20 20:32:54 +0900

    Update with new features

--- Lab-s/1 - Cube/Console-Project/Core.cs
+++ Lab-s/1 - Cube/Console-Project/Core.cs
@@ -1,94 +1,62 @@
 using OpenTK.Graphics.OpenGL4;
-using OpenTK.Mathematics;
 using OpenTK.Windowing.Common;
 using OpenTK.Windowing.Desktop;
 
 namespace Console_Project
 {
-
     public class Core : GameWindow
     {
-        Shader shader;
-        Polygon polygon;
-        int vertexBufferObject, vertexArrayObject;
+        public readonly static int Vec3AttributeSize = 3;
+        readonly OpenGLDrawFigure figure;
+        readonly Shader shader;
 
-        public Core(int width = 800, int height = 640, string title = "Cube")
+        public Core(
+            string title,
+            int width = 800,
+            int height = 640,
+            VSyncMode vSyncMode = VSyncMode.On
+        )
             : base(
                 GameWindowSettings.Default,
-                new NativeWindowSettings() { Size = (width, height), Title = title }
+                new NativeWindowSettings()
+                {
+                    Size = (width, height),
+                    Title = title,
+                    Vsync = vSyncMode,
+                    Flags = ContextFlags.ForwardCompatible
+                }
             )
         {
-            polygon = new Polygon(new Vector3[] {
-                new Vector3( -.5f, .5f, 0f ),
-                // new Vector3( .5f, .5f, 0f ),
-                new Vector3( .5f, -.5f, 0f ),
-                new Vector3( -.5f, -.5f, 0f )
-            }
-            );
+            shader = new Shader("shader.vert", "shader.frag");
+            figure = new OpenGLDrawFigure(Square.TestSquare, shader);
         }
-
 
         protected override void OnLoad()
         {
             base.OnLoad();
-            GL.ClearColor(.18f, .18f, .21f, 1f);
-
-            vertexBufferObject = GL.GenBuffer();
-            GL.BindBuffer(BufferTarget.ArrayBuffer, vertexBufferObject);
-            var vertices = polygon.Vertices;
-            var count = polygon.VerticesCount;
-
-            GL.BufferData(BufferTarget.ArrayBuffer,
-            count * sizeof(float),
-            vertices, BufferUsageHint.StaticDraw);
-
-            vertexArrayObject = GL.GenVertexArray();
-            GL.BindVertexArray(vertexArrayObject);
-            var index = 0;
-            GL.VertexAttribPointer(index, count, VertexAttribPointerType.Float, false, count * sizeof(float), 0); 
-            GL.EnableVertexAttribArray(index); 
-
-
-            shader = new Shader("shader.vert", "shader.frag");
-            shader.Use();
+            GL.ClearColor(.1f, .1f, .15f, 1f);
+            figure.Init();
         }
 
-        void Draw()
+        protected override void OnUpdateFrame(FrameEventArgs args)
         {
-            shader.Use();
-            GL.BindVertexArray(vertexArrayObject);
-            // TODO: display square
-            GL.DrawArrays(PrimitiveType.Triangles, 0, polygon.VerticesCount);
+            base.OnUpdateFrame(args);
         }
 
-        public override void Run()
-        {
-            base.Run();
-        }
-
-        protected override void OnUpdateFrame(FrameEventArgs e)
-        {
-            base.OnUpdateFrame(e);
-        }
-
-        protected override void OnUnload()
-        {
-            shader.Dispose();
-        }
         protected override void OnRenderFrame(FrameEventArgs args)
         {
             base.OnRenderFrame(args);
             GL.Clear(ClearBufferMask.ColorBufferBit);
 
-            Draw();
+            figure.Draw();
+
             SwapBuffers();
         }
 
-        protected override void OnResize(ResizeEventArgs e)
+        protected override void OnUnload()
         {
-            base.OnResize(e);
-            GL.Viewport(0, 0, e.Width, e.Height);
+            figure.Dispose();
+            base.OnUnload();
         }
     }
-
 }
