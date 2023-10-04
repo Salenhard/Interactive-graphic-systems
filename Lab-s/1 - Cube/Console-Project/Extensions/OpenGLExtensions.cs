8156a944e60c9a949aa28c57670f0eaf62059c46
NemoNology <chicken.gang@mail.ru>
2023-10-04 19:29:22 +0900

    Added new method of gameObject init by figure

--- Lab-s/1 - Cube/Console-Project/Extensions/OpenGLExtensions.cs
+++ Lab-s/1 - Cube/Console-Project/Extensions/OpenGLExtensions.cs
@@ -2,7 +2,7 @@ using OpenTK.Graphics.OpenGL;
 
 namespace Console_Project
 {
-    public class OpenGLExtensions
+    public static class OpenGLExtensions
     {
         public static readonly int Ver2AttributeSize = 2,
             Ver3AttributeSize = 3;
@@ -40,7 +40,7 @@ namespace Console_Project
         )
         {
             var vbo = CreateVBO(vertices, verticesTypeSize, bufferUsageHint); // vbo = Gen, Bind and Buffer
-            var vao = GL.GenVertexArray(); // VertexArrayHandler = GL.GenVertexArray();
+            var vao = GL.GenVertexArray();
             // TODO: [0] Why this zero? What's that means?
             var zero = 0;
             GL.BindVertexArray(vao);
@@ -74,5 +74,59 @@ namespace Console_Project
             );
             return ebo;
         }
+
+        public static (
+            int VertexBufferObject,
+            int VertexArrayObject,
+            int ElementBufferObject
+        ) InitAll(
+            this Figure figure,
+            BufferUsageHint bufferUsageHint = BufferUsageHint.StaticDraw,
+            int vertexStride = 3
+        )
+        {
+            var vertices = figure.VerticesCoordinates;
+            var indices = figure.Indices;
+
+            var verticesTypeSize = sizeof(float);
+            var indicesTypeSize = sizeof(uint);
+
+            var vbo = GL.GenBuffer();
+            GL.BindBuffer(BufferTarget.ArrayBuffer, vbo);
+            GL.BufferData(
+                BufferTarget.ArrayBuffer,
+                vertices.Length * verticesTypeSize,
+                vertices,
+                bufferUsageHint
+            );
+
+            var vao = GL.GenVertexArray();
+            // TODO: [0] Why this zero? What's that means?
+            var zero = 0;
+            GL.BindVertexArray(vao);
+            GL.VertexAttribPointer(
+                zero,
+                Ver3AttributeSize,
+                VertexAttribPointerType.Float,
+                false,
+                vertexStride * sizeof(float),
+                0
+            );
+            // TODO: [0] ...
+            GL.EnableVertexAttribArray(zero);
+
+            var ebo = GL.GenBuffer();
+            GL.BindBuffer(BufferTarget.ElementArrayBuffer, ebo);
+            GL.BufferData(
+                BufferTarget.ElementArrayBuffer,
+                indices.Length * indicesTypeSize,
+                indices,
+                bufferUsageHint
+            );
+
+            return (vbo, vao, ebo);
+        }
+
+        public static GameObject ToGameObject(this Figure figure) => new(figure);
     };
 }
