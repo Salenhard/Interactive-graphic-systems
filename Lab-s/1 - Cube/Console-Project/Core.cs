c277ad925aff19ff22728af49be09fdc4cbbc8fb
NemoNology <chicken.gang@mail.ru>
2023-10-07 11:05:45 +0900

    Tested uniform input

--- Lab-s/1 - Cube/Console-Project/Core.cs
+++ Lab-s/1 - Cube/Console-Project/Core.cs
@@ -12,6 +12,8 @@ namespace Console_Project
         public const int WIDTH = 800;
         public const int HEIGHT = 640;
 
+        float test = 0f;
+
         public Core(
             string title,
             int width = WIDTH,
@@ -29,24 +31,35 @@ namespace Console_Project
                 }
             )
         {
-            gameObjectsControllers.Add(
-                new(
-                    ShaderProgram.PerspectiveSimple.ShaderProgramHandler,
-                    Figure.TestCube2.ToGameObject()
-                )
-            );
+            ShaderProgram temp = ShaderProgram.PerspectiveUniform;
 
-            gameObjectsControllers.Add(
-                new(
-                    ShaderProgram.Default.ShaderProgramHandler,
-                    Figure.CreateSquare(new(-.4f, -.4f, 0.0f), .3f).ToGameObject(),
-                    Figure.CreateSquare(new(-.4f, +.4f, 0.0f), .3f).ToGameObject(),
-                    Figure.CreateSquare(new(+.4f, -.4f, 0.0f), .3f).ToGameObject(),
-                    Figure.CreateSquare(new(+.4f, +.4f, 0.0f), .3f).ToGameObject()
-                )
-            );
+            Test(temp);
+
+            gameObjectsControllers.Add(new(temp, Figure.TestCube2.ToGameObject()));
 
             CenterWindow();
+        }
+
+        void Test(ShaderProgram shaderProgram)
+        {
+            shaderProgram.SetUniform("iColor", new Vector3(.5f, .5f, .5f));
+            shaderProgram.SetUniform(
+                "iModel",
+                Matrix4.CreateTranslation(0f, 0f, -5f) * Matrix4.CreateRotationZ(test)
+            );
+
+            test += .01f;
+
+            shaderProgram.SetUniform("iView", Matrix4.CreateTranslation(0f, 0f, 0f));
+            shaderProgram.SetUniform(
+                "iProjection",
+                Matrix4.CreatePerspectiveFieldOfView(
+                    MathHelper.DegreesToRadians(45f),
+                    1f * WIDTH / HEIGHT,
+                    1f,
+                    10f
+                )
+            );
         }
 
         protected override void OnLoad()
@@ -58,6 +71,9 @@ namespace Console_Project
         protected override void OnUpdateFrame(FrameEventArgs args)
         {
             base.OnUpdateFrame(args);
+            CheckInputs();
+
+            Test(gameObjectsControllers[0].ShaderProgram);
         }
 
         protected override void OnRenderFrame(FrameEventArgs args)
@@ -86,6 +102,14 @@ namespace Console_Project
         {
             base.OnResize(e);
             GL.Viewport(0, 0, e.Width, e.Height);
+        }
+
+        async void CheckInputs()
+        {
+            if (MouseState.IsAnyButtonDown)
+            {
+                InputsController.InvokeLMBClick(this, EventArgs.Empty);
+            }
         }
     }
 }
