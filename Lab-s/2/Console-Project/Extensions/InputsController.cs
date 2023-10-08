4e946a1d4fad9342e2c76b61762fcf3ed216a7e7
NemoNology <chicken.gang@mail.ru>
2023-10-09 08:30:26 +0900

    Added Left and Right controls handler

--- Lab-s/2/Console-Project/Extensions/InputsController.cs
+++ Lab-s/2/Console-Project/Extensions/InputsController.cs
@@ -3,11 +3,25 @@ namespace Console_Project
     partial class InputsController
     {
         public static event EventHandler? OnLMBClick;
+        public static event EventHandler? OnLeftPress;
+        public static event EventHandler? OnRightPress;
 
         public static void InvokeLMBClick(object sender, EventArgs? eventArgs)
         {
             eventArgs ??= EventArgs.Empty;
             OnLMBClick?.Invoke(sender, eventArgs);
+        }
+
+        public static void InvokeLeftPress(object sender, EventArgs? eventArgs)
+        {
+            eventArgs ??= EventArgs.Empty;
+            OnLeftPress?.Invoke(sender, eventArgs);
+        }
+
+        public static void InvokeRightPress(object sender, EventArgs? eventArgs)
+        {
+            eventArgs ??= EventArgs.Empty;
+            OnRightPress?.Invoke(sender, eventArgs);
         }
     }
 }
