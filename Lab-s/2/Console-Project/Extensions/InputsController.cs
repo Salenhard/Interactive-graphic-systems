ad51eb5b629da8b987b19166b83e85607ef2e77f
NemoNology <chicken.gang@mail.ru>
2023-10-09 10:19:30 +0900

    Removed extra invokes

--- Lab-s/2/Console-Project/Extensions/InputsController.cs
+++ Lab-s/2/Console-Project/Extensions/InputsController.cs
@@ -3,25 +3,11 @@ namespace Console_Project
     partial class InputsController
     {
         public static event EventHandler? OnLMBClick;
-        public static event EventHandler? OnLeftPress;
-        public static event EventHandler? OnRightPress;
 
-        public static void InvokeLMBClick(object sender, EventArgs? eventArgs)
+        public static void InvokeLMBClick(object sender, EventArgs? eventArgs = null)
         {
             eventArgs ??= EventArgs.Empty;
             OnLMBClick?.Invoke(sender, eventArgs);
-        }
-
-        public static void InvokeLeftPress(object sender, EventArgs? eventArgs)
-        {
-            eventArgs ??= EventArgs.Empty;
-            OnLeftPress?.Invoke(sender, eventArgs);
-        }
-
-        public static void InvokeRightPress(object sender, EventArgs? eventArgs)
-        {
-            eventArgs ??= EventArgs.Empty;
-            OnRightPress?.Invoke(sender, eventArgs);
         }
     }
 }
