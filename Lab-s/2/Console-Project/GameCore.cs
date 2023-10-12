e2f6c90ab6bbda428a40ace9f22ee60650f19ac6
NemoNology <chicken.gang@mail.ru>
2023-10-12 10:58:05 +0900

    Updated with new featured - B 0.85

--- Lab-s/2/Console-Project/GameCore.cs
+++ Lab-s/2/Console-Project/GameCore.cs
@@ -3,6 +3,7 @@ using OpenTK.Windowing.Common;
 using OpenTK.Windowing.Desktop;
 using OpenTK.Windowing.GraphicsLibraryFramework;
 using OpenTK.Mathematics;
+using StbImageSharp;
 
 namespace Console_Project
 {
@@ -11,80 +12,75 @@ namespace Console_Project
         #region Constants
         public const int WIDTH = 800;
         public const int HEIGHT = 640;
+        public const string GameTitle = "Fast clicky!";
         #endregion
 
         #region Variables
-        Dictionary<string, GameObjectsController> gameObjectsControllers = new();
+        GameObjectsController gameObjectsController = null!;
         readonly Dictionary<string, float> floatValues = new();
         readonly Dictionary<string, bool> boolValues = new();
-        readonly Dictionary<string, int> intValues = new();
+        readonly Dictionary<string, Vector3> vector3Values = new();
         readonly System.Diagnostics.Stopwatch timer = System.Diagnostics.Stopwatch.StartNew();
-        ShaderProgram mainShader = null!;
-        StateSystem stateSystem = null!;
         #endregion
 
-        public GameCore(
-            string title,
-            int width = WIDTH,
-            int height = HEIGHT,
-            VSyncMode vSyncMode = VSyncMode.On
-        )
+        public GameCore(int width = WIDTH, int height = HEIGHT, VSyncMode vSyncMode = VSyncMode.On)
             : base(
                 GameWindowSettings.Default,
                 new NativeWindowSettings()
                 {
                     Size = (width, height),
-                    Title = title,
+                    Title = GameTitle,
                     Vsync = vSyncMode,
                     Flags = ContextFlags.ForwardCompatible
                 }
             )
         {
             Init();
-
             CenterWindow();
         }
 
         #region Overrides functions
         protected override void OnLoad()
         {
-            base.OnLoad();
             GL.ClearColor(.5f, .6f, .6f, 1f);
-
-            floatValues.Add("RotationSlow", 0f);
-            floatValues.Add("RotationMedium", 0f);
-            floatValues.Add("Score", 1f);
-            floatValues.Add("ScoreDecreasing", 0.01f);
-            boolValues.Add("IsRageMode", false);
+            GL.Enable(EnableCap.DepthTest);
+            // StbImage.stbi_set_flip_vertically_on_load(1);
+            // GL.TexImage2D(
+            //     TextureTarget.Texture2D,
+            //     0,
+            //     PixelInternalFormat.Rgba,
+            //     image.Width,
+            //     image.Height,
+            //     0,
+            //     PixelFormat.Rgba,
+            //     PixelType.UnsignedByte,
+            //     image.Data
+            // );
+            base.OnLoad();
         }
 
         protected override void OnUpdateFrame(FrameEventArgs args)
         {
+            Update();
             base.OnUpdateFrame(args);
-
-            OnUpdate();
         }
 
         protected override void OnRenderFrame(FrameEventArgs args)
         {
-            base.OnRenderFrame(args);
-            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
-
-            foreach (var item in gameObjectsControllers)
+            if (!boolValues["IsPause"])
             {
-                item.Value.Draw();
-            }
+                GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
 
-            SwapBuffers();
+                gameObjectsController.Draw();
+
+                SwapBuffers();
+                base.OnRenderFrame(args);
+            }
         }
 
         protected override void OnUnload()
         {
-            foreach (var item in gameObjectsControllers)
-            {
-                item.Value.Dispose();
-            }
-
+            gameObjectsController.Dispose();
             base.OnUnload();
         }
 
@@ -94,124 +90,136 @@ namespace Console_Project
 
             var (w, h) = ((float)WIDTH / e.Width, (float)HEIGHT / e.Height);
 
-            mainShader.SetUniform(
-                "iScale",
-                (
-                    // Matrix4.CreateScale(w, h, 1),
-                    Matrix4.CreateScale(w, h, 1),
-                    ActiveUniformType.FloatMat4
-                ),
-                true
+            var mainObject = gameObjectsController.GameObjectsAndTheirShaderUniformsValues[0];
+
+            mainObject.ShaderUniformValuesInfos["iScale"] = (
+                Matrix4.CreateScale(w, h, 1),
+                ActiveUniformType.FloatMat4
             );
+
             GL.Viewport(0, 0, e.Width, e.Height);
         }
         #endregion
 
         #region Facade
-        void OnUpdate()
-        {
-            CheckInputs();
-            UpdateMainShaderValues(boolValues["IsRageMode"]);
-
-            foreach (var item in gameObjectsControllers)
-            {
-                item.Value.Update();
-            }
-        }
-
         void Init()
         {
-            mainShader = ShaderProgram.PerspectiveUniform;
+            floatValues.Add("RotationX", 0f);
+            floatValues.Add("RotationYandZ", 0f);
+            floatValues.Add("RotationSlowStep", MathExtension.SmallStep);
+            floatValues.Add("RotationMediumStep", MathExtension.BigStep);
+            floatValues.Add("Score", -1f);
+            floatValues.Add("ScoreDecreasing", MathExtension.SmallStep);
+            floatValues.Add("ScoreEncreasing", MathExtension.BigStep * 1.5f);
+            boolValues.Add("IsRageMode", false);
+            boolValues.Add("IsPause", false);
+            vector3Values.Add("ScoreBarNormalColor", new Vector3(0f, 0.28f, 0.67f));
+            vector3Values.Add("ScoreBarRageColor", new Vector3(0.82f, 0.1f, 0.1f));
+            vector3Values.Add("ScoreBarColor", vector3Values["ScoreBarNormalColor"]);
 
-            (string UniformName,
-                dynamic UniformValue, 
-                ActiveUniformType UniformType)[] mainShaderUniformValues = {
-                    ("iMVP",
-                    Matrix4.CreateTranslation(0f, 0f, 0f)
-                        * Matrix4.LookAt(new(0f, 0f, 5f), Vector3.Zero, Vector3.UnitY)
-                        * Matrix4.CreatePerspectiveFieldOfView(
-                            MathHelper.DegreesToRadians(45f),
-                            1f * WIDTH / HEIGHT,
-                            1f,
-                            10f
-                        ),
-                    ActiveUniformType.FloatMat4),
-                    ("iScale", Matrix4.Identity, ActiveUniformType.FloatMat4)
-                };
+            // TODO: Add Loose/Win "screen" display
+            // var texture = StbImageSharp
 
-            (string UniformName,
-                dynamic UniformValue, 
-                ActiveUniformType UniformType)[] hintLooseDisplayShaderUniformValues = {
-                    ("iColor", new Vector4(0f, 1f, 0f, 0f), ActiveUniformType.FloatVec4)
-                };
+            Restart();
 
-            (string UniformName,
-                dynamic UniformValue, 
-                ActiveUniformType UniformType)[] hintWinDisplayShaderUniformValues = {
-                    ("iColor", new Vector4(1f, 1f, 1f, 0f), ActiveUniformType.FloatVec4)
-                };
+            var mat4Type = ActiveUniformType.FloatMat4;
+            var identity = (Matrix4.Identity, mat4Type);
 
-            InitShaderUniforms(mainShader, mainShaderUniformValues);
-
-            gameObjectsControllers.Add("Main", new(mainShader, Figure.TestCube2.ToGameObject(BufferUsageHint.StreamDraw)));
-            gameObjectsControllers.Add("HintLooseDisplay", new(ShaderProgram.Colorable, 
-                Figure.CreateSquare(Vector3.Zero, 2f).ToGameObject()));
-            // gameObjectsControllers.Add("HintWinDisplay", new(ShaderProgram.Colorable, 
-            //     Figure.CreateSquare(Vector3.Zero, 2f).ToGameObject()));
-
-            InitShaderUniforms(gameObjectsControllers["HintLooseDisplay"].ShaderProgram, 
-                hintLooseDisplayShaderUniformValues);
-            // InitShaderUniforms(gameObjectsControllers["HintWinDisplay"].ShaderProgram, 
-            //     hintWinDisplayShaderUniformValues);
-            
-            stateSystem = new StateSystem(3);
+            Dictionary<
+                string,
+                (dynamic UniformValue, ActiveUniformType UniformType)
+            > mainObjectUniformValues =
+                    new()
+                    {
+                        {
+                            "iMVP",
+                            (
+                                Matrix4.CreateTranslation(0f, 0f, 0f)
+                                    * Matrix4.LookAt(new(0f, 0f, 5f), Vector3.Zero, Vector3.UnitY)
+                                    * Matrix4.CreatePerspectiveFieldOfView(
+                                        MathHelper.DegreesToRadians(45f),
+                                        1f * WIDTH / HEIGHT,
+                                        1f,
+                                        10f
+                                    ),
+                                mat4Type
+                            )
+                        },
+                        { "iScale", identity },
+                        { "iIsUsingInputColor", (0, ActiveUniformType.Int) },
+                        { "iTranslation", identity }
+                    },
+                scoreBarUniformValues =
+                    new()
+                    {
+                        {
+                            "iTranslation",
+                            (Matrix4.CreateTranslation(floatValues["Score"], -1.75f, 0f), mat4Type)
+                        },
+                        { "iMVP", identity },
+                        { "iRotationX", identity },
+                        { "iRotationY", identity },
+                        { "iRotationZ", identity },
+                        { "iIsUsingInputColor", (1, ActiveUniformType.Int) },
+                        { "iColor", (vector3Values["ScoreBarColor"], ActiveUniformType.FloatVec3) }
+                    };
+            gameObjectsController = new(
+                ShaderProgram.OurGameUniformShader,
+                (
+                    OpenGLFigure.MainCube.ToGameObject(BufferUsageHint.DynamicDraw),
+                    mainObjectUniformValues
+                ),
+                (
+                    OpenGLFigure
+                        .CreateSquare(Vector3.Zero, 2f)
+                        .ToGameObject(BufferUsageHint.DynamicDraw),
+                    scoreBarUniformValues
+                )
+            );
         }
         #endregion
 
         #region Necessary logic function
-        void InitShaderUniforms(ShaderProgram shaderProgram, (string UniformName, dynamic UniformValue, ActiveUniformType UniformType)[] uniformsInfo)
-        {
-            foreach (var uniformInfo in uniformsInfo)
-            {
-                shaderProgram.SetUniform(
-                    uniformInfo.UniformName,
-                    (
-                        uniformInfo.UniformValue,
-                        uniformInfo.UniformType
-                    )
-                );
-            }
-        }
-
         void CheckInputs()
         {
             if (MouseState.IsButtonPressed(MouseButton.Button1))
             {
-                InputsController.InvokeLMBClick(this, EventArgs.Empty);
+                floatValues["Score"] += floatValues["ScoreEncreasing"];
             }
             if (MouseState.IsButtonPressed(MouseButton.Button2))
             {
-                boolValues["IsRageMode"] = true;
+                Rage_UnRage();
             }
-            if (MouseState.IsButtonReleased(MouseButton.Button2))
+            if (KeyboardState.IsKeyPressed(Keys.R))
             {
-                boolValues["IsRageMode"] = false;
+                Restart();
+            }
+            if (KeyboardState.IsKeyPressed(Keys.P | Keys.Space))
+            {
+                Pause_Unpause();
             }
         }
 
-        void UpdateMainShaderValues(bool isRageMode = false)
+        void Update()
         {
-            UpdateRotationValues(isRageMode);
-            UpdateColorValues(isRageMode);
-            UpdateHintDisplayTransparancy();
+            CheckInputs();
+
+            if (!boolValues["IsPause"])
+            {
+                UpdateScore();
+                UpdateMainShaderValues();
+
+                gameObjectsController.Update();
+            }
         }
 
-        void UpdateHintDisplayTransparancy()
+        void UpdateMainShaderValues()
         {
-            
+            UpdateRotationValues();
+            UpdateColorValues();
         }
 
-        void UpdateRotationValues(bool isRageMode = false)
+        void UpdateRotationValues()
         {
             foreach (var f in floatValues)
             {
@@ -221,49 +229,122 @@ namespace Console_Project
                 }
             }
 
-            if (isRageMode)
-            {
-                floatValues["RotationSlow"] += MathExtension.BigStep;
-                floatValues["RotationMedium"] += MathExtension.KiloStep;
-            }
-            else
-            {
-                floatValues["RotationSlow"] += MathExtension.SmallStep;
-                floatValues["RotationMedium"] += MathExtension.MediumStep;
-            }
+            var mainObject = gameObjectsController.GameObjectsAndTheirShaderUniformsValues[0];
 
-            mainShader.SetUniform(
-                "iRotationX",
-                (Matrix4.CreateRotationX(floatValues["RotationMedium"]), ActiveUniformType.FloatMat4),
-                true
+            floatValues["RotationX"] += floatValues["RotationMediumStep"];
+            floatValues["RotationYandZ"] += floatValues["RotationSlowStep"];
+
+            var slowRotationMatrix = Matrix4.CreateRotationY(floatValues["RotationYandZ"]);
+            var mat4Type = ActiveUniformType.FloatMat4;
+
+            mainObject.ShaderUniformValuesInfos["iRotationX"] = (
+                Matrix4.CreateRotationX(floatValues["RotationX"]),
+                mat4Type
             );
-            mainShader.SetUniform(
-                "iRotationY",
-                (Matrix4.CreateRotationY(floatValues["RotationSlow"]), ActiveUniformType.FloatMat4),
-                true
-            );
-            mainShader.SetUniform(
-                "iRotationZ",
-                (Matrix4.CreateRotationY(floatValues["RotationSlow"]), ActiveUniformType.FloatMat4),
-                true
-            );
+            mainObject.ShaderUniformValuesInfos["iRotationY"] = (slowRotationMatrix, mat4Type);
+            mainObject.ShaderUniformValuesInfos["iRotationZ"] = (slowRotationMatrix, mat4Type);
         }
 
-        void UpdateColorValues(bool isRageMode = false)
+        void UpdateColorValues()
         {
-            var seconds = (float)timer.Elapsed.TotalSeconds * (isRageMode ? 5 : 1);
+            var seconds = (float)timer.Elapsed.TotalSeconds * (boolValues["IsRageMode"] ? 5 : 1);
 
-            mainShader.SetUniform(
-                "iResolution",
-                (new Vector2(Size.X, Size.Y), ActiveUniformType.FloatVec2),
-                true
+            var mainObject = gameObjectsController.GameObjectsAndTheirShaderUniformsValues[0];
+            var scoreBarObject = gameObjectsController.GameObjectsAndTheirShaderUniformsValues[1];
+
+            mainObject.ShaderUniformValuesInfos["iResolution"] = (
+                new Vector2(Size.X, Size.Y),
+                ActiveUniformType.FloatVec2
             );
 
-            mainShader.SetUniform("iTime", (seconds, ActiveUniformType.Float), true);
+            mainObject.ShaderUniformValuesInfos["iTime"] = (seconds, ActiveUniformType.Float);
+
+            scoreBarObject.ShaderUniformValuesInfos["iColor"] = (
+                vector3Values["ScoreBarColor"],
+                ActiveUniformType.FloatVec3
+            );
 
             if (seconds > 1024_000f)
             {
                 timer.Restart();
+            }
+        }
+
+        void UpdateScore()
+        {
+            var scoreBarObject = gameObjectsController.GameObjectsAndTheirShaderUniformsValues[1];
+
+            floatValues["Score"] -= floatValues["ScoreDecreasing"];
+
+            scoreBarObject.ShaderUniformValuesInfos["iTranslation"] = (
+                Matrix4.CreateTranslation(floatValues["Score"], -1.75f, 0f),
+                ActiveUniformType.FloatMat4
+            );
+
+            CheckGameState();
+        }
+
+        void CheckGameState()
+        {
+            if (floatValues["Score"] >= 0f)
+            {
+                System.Console.WriteLine("You win");
+                boolValues["IsPause"] = true;
+            }
+            else if (floatValues["Score"] <= -2f)
+            {
+                System.Console.WriteLine("You Loose");
+                boolValues["IsPause"] = true;
+            }
+        }
+
+        void Restart()
+        {
+            boolValues["IsPause"] = false;
+            // Pause_Unpause();
+            boolValues["IsRageMode"] = true;
+            Rage_UnRage();
+            floatValues["Score"] = -1f;
+            floatValues["RotationX"] = 0f;
+            floatValues["RotationYandZ"] = 0f;
+            timer.Restart();
+        }
+
+        void Pause_Unpause()
+        {
+            boolValues["IsPause"] = !boolValues["IsPause"];
+
+            if (boolValues["IsPause"])
+            {
+                timer.Stop();
+            }
+            else
+            {
+                timer.Start();
+            }
+        }
+
+        void Rage_UnRage()
+        {
+            boolValues["IsRageMode"] = !boolValues["IsRageMode"];
+
+            floatValues["ScoreEncreasing"] = MathExtension.BigStep;
+
+            if (boolValues["IsRageMode"])
+            {
+                floatValues["RotationSlowStep"] = MathExtension.BigStep;
+                floatValues["RotationMediumStep"] = MathExtension.KiloStep;
+                floatValues["ScoreDecreasing"] = MathExtension.SmallStep * 4;
+                floatValues["ScoreEncreasing"] *= 3.5f;
+                vector3Values["ScoreBarColor"] = vector3Values["ScoreBarRageColor"];
+            }
+            else
+            {
+                floatValues["RotationSlowStep"] = MathExtension.SmallStep;
+                floatValues["RotationMediumStep"] = MathExtension.MediumStep;
+                floatValues["ScoreDecreasing"] = MathExtension.SmallStep;
+                floatValues["ScoreEncreasing"] *= 1.5f;
+                vector3Values["ScoreBarColor"] = vector3Values["ScoreBarNormalColor"];
             }
         }
         #endregion
