0bcafbc9964715c28b8fad0df1bed0214ca9fdbb
NemoNology <chicken.gang@mail.ru>
2024-02-15 19:53:38 +0900

    Main scene:
    Changed:
    > Rusificated
    Added:
    + Titles, settings buttons

--- Lab-s/3/Godot/Scripts/main_menu_ui.gd
+++ Lab-s/3/Godot/Scripts/main_menu_ui.gd
@@ -9,5 +9,9 @@ func _on_game_rules_pressed():
 	get_tree().change_scene_to_file("res://Scenes/game_rules.tscn")
 
 
-func _on_models_pressed():
-	get_tree().change_scene_to_file("res://Scenes/models.tscn")
+func _on_titles_pressed():
+	get_tree().change_scene_to_file("res://Scenes/titles.tscn")
+
+
+func _on_settings_pressed():
+	get_tree().change_scene_to_file("res://Scenes/settings.tscn")
