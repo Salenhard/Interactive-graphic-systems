d029e7dade259266f421d5db814298b476d6a0dd
NemoNology <chicken.gang@mail.ru>
2024-02-23 17:18:04 +0900

    Model 3000 class - B 1.0:
    Added:
    + Model scale property
    + Disposing by timer
    + Adding several meshes from model (.gbl)
    + Collision shape adding for collusions detection

--- Lab-s/3/Godot/Scripts/model_3000.gd
+++ Lab-s/3/Godot/Scripts/model_3000.gd
@@ -1,15 +1,18 @@
 class_name Model3000 extends RigidBody3D
 
-var dispose_timer: Timer
+var dispose_timer: Timer = Timer.new()
 @export_category("Model3000")
-## model path is path from root path
+## model path is path from project root path
 @export var model_path: String = "res://"
 @export var dispose_seconds: int = 1
+@export var models_scale: Vector3 = Vector3.ONE
+
+
+func _init():
+	dispose_timer.timeout.connect(on_timeout)
 
 
 func load_model():
-	dispose_timer = Timer.new()
-	dispose_timer.wait_time = dispose_seconds
 	var scene: PackedScene = Model3000.load_glft_document(model_path)
 	if scene != null:
 		var model_node: Node = scene.instantiate()
@@ -19,12 +22,24 @@ func load_model():
 				m.mesh = c.mesh
 				m.skin = c.skin
 				m.skeleton = c.skeleton
+				m.scale = models_scale
 				m.create_convex_collision()
+				for mc in m.get_children():
+					if mc is StaticBody3D:
+						for mcc in mc.get_children():
+							if mcc is CollisionShape3D:
+								var shape_buffer: CollisionShape3D = mcc.duplicate(true)
+								shape_buffer.scale = models_scale
+								add_child(shape_buffer)
+						m.remove_child(mc)
 				add_child(m)
 
 
 func _ready():
 	load_model()
+	dispose_timer.wait_time = dispose_seconds
+	add_child(dispose_timer)
+	dispose_timer.start()
 
 
 static func load_glft_document(glft_document_path: String) -> PackedScene:
@@ -43,3 +58,7 @@ static func load_glft_document(glft_document_path: String) -> PackedScene:
 
 	printerr("Couldn't load glTF scene (error code: %s)." % error_string(error))
 	return null
+
+
+func on_timeout():
+	queue_free()
