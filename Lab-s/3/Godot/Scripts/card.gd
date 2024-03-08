e9e5e41ada49316ca2cfda2c32a341918581b62c
NemoNology <chicken.gang@mail.ru>
2024-03-08 22:17:09 +0900

    Card class:
    Fixed:
    * ID display
    Added:
    + Hover animation
    + Signal "toogled" emitting
    Removed:
    - Toogle possibility if card face is up

--- Lab-s/3/Godot/Scripts/card.gd
+++ Lab-s/3/Godot/Scripts/card.gd
@@ -3,6 +3,7 @@ class_name Card extends BaseButton
 static var sprite_front: Texture2D = load("res://Assets/Images/card_front_texture.png")
 static var sprite_back: Texture2D = load("res://Assets/Images/card_back_texture.png")
 const DEFAULT_SIZE: Vector2 = Vector2(50, 75)
+static var hover_transform: Transform2D = Transform2D(0, Vector2(.9, .9), 0, Vector2(2, 4))
 
 @export var is_card_up:bool = false
 @export var id:int = -1
@@ -16,11 +17,11 @@ func _init():
 	size = custom_minimum_size
 	sprite.centered = false
 	label.add_theme_color_override("font_color", Color.BLACK)
-	label.text = str(id)
 	# TODO: change magic number 56 to number that calculated by card size
 	label.add_theme_font_size_override("font_size", 28)
 
 func _ready():
+	label.text = str(id)
 	add_child(sprite)
 	update_cart_texture()
 	var cont = CenterContainer.new()
@@ -38,7 +39,17 @@ func update_cart_texture():
 
 func toogle():
 	is_card_up = !is_card_up
+	toogled.emit(id, is_card_up)
 	update_cart_texture()
 	
 func _pressed():
-	toogle()
+	if not is_card_up:
+		toogle()
+
+func _process(delta):
+	if is_hovered() and not is_card_up:
+		sprite.transform = hover_transform
+		sprite.self_modulate = Color(1.25, 1.25, 1.25)
+	else:
+		sprite.transform = Transform2D.IDENTITY
+		sprite.self_modulate = Color.WHITE
